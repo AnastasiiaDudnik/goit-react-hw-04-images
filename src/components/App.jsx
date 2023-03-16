48e54ffb24714a2a6f971ecf290c3c761c97df7b
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-03-16 20:55:56 +0200

    gallery refactor with mistake

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,72 @@ import { Toaster } from 'react-hot-toast';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { SearchBar } from './Searchbar/Searchbar';
 import { Container } from './App.styles';
+import { pixabayApi } from 'services/pixabayAPI';
+import { LoadMoreButton } from './Button/LoadMoreButton';
+import { Loader } from './Loader/Loader';
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REGECTED: 'regected',
+};
 
 export class App extends Component {
   state = {
     searchQuerry: '',
+    images: [],
+    error: '',
+    status: Status.IDLE,
+    page: 1,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuerry, page } = this.state;
+
+    if (prevProps.searchQuerry !== searchQuerry || prevState.page !== page) {
+      this.setState({ status: Status.PENDING });
+
+      pixabayApi(searchQuerry.trim(), page)
+        .then(images =>
+          this.setState({
+            images: [...this.state.images, ...images.hits],
+            status: Status.RESOLVED,
+          })
+        )
+
+        .catch(error => this.setState({ error, status: Status.REGECTED }));
+    }
+
+    if (prevProps.searchQuerry !== searchQuerry) {
+      this.setState({ images: [], page: 1 });
+    }
+  }
 
   onFormSubmit = searchQuerry => {
     this.setState({ searchQuerry });
   };
 
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
-    const { searchQuerry } = this.state;
+    const { images, error, status } = this.state;
 
     return (
       <Container>
         <Toaster />
         <SearchBar onSubmit={this.onFormSubmit} />
-        <ImageGallery searchQuerry={searchQuerry} onClick={this.toggleModal} />
+
+        {status.PENDING && <Loader />}
+        {status.RESOLVED && (
+          <>
+            <ImageGallery images={images} />
+            <LoadMoreButton onClick={this.onLoadMore} />
+          </>
+        )}
+        {status.REGECTED && <h1>{error.message}</h1>}
       </Container>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,69 +1,14 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Loader } from 'components/Loader/Loader';
-import { pixabayApi } from 'services/pixabayAPI';
-import { LoadMoreButton } from 'components/Button/LoadMoreButton';
+// import { Loader } from 'components/Loader/Loader';
+// import { pixabayApi } from 'services/pixabayAPI';
+// import { LoadMoreButton } from 'components/Button/LoadMoreButton';
 import { Gallery } from './ImageGallery.styled';
 
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-    error: '',
-    status: 'idle',
-    page: 1,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuerry } = this.props;
-    const { page } = this.state;
-
-    if (prevProps.searchQuerry !== searchQuerry || prevState.page !== page) {
-      this.setState({ status: 'pending' });
-
-      pixabayApi(searchQuerry.trim(), page)
-        .then(images =>
-          this.setState({
-            images: [...this.state.images, ...images.hits],
-            status: 'resolved',
-          })
-        )
-
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-
-    if (prevProps.searchQuerry !== searchQuerry) {
-      this.setState({ images: [] });
-    }
-  }
-
-  onLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  render() {
-    const { images, error, status } = this.state;
-
-    if (status === 'pending') return <Loader />;
-
-    if (status === 'pending' && images.length > 0) {
-      return (
-        <Gallery>
-          <ImageGalleryItem images={images} />
-          <Loader />
-        </Gallery>
-      );
-    }
-
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-
-    if (status === 'resolved')
-      return (
-        <Gallery>
-          <ImageGalleryItem images={images} />
-          <LoadMoreButton onClick={this.onLoadMore} />
-        </Gallery>
-      );
-  }
+export function ImageGallery({ images }) {
+  return (
+    <Gallery>
+      <ImageGalleryItem images={images} />
+    </Gallery>
+  );
 }
